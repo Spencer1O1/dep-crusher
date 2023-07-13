2e7766859bd96b2993ea0026161b67d5e2415c2d
Spencer Smith <spencerliamsmith@gmail.com>
2023-07-12 20:30:25 -0600

    Removed unnecessary traits

--- src/dep_node.rs
+++ src/dep_node.rs
@@ -1,11 +1,10 @@
-use std::fmt::Debug;
 use std::hash::Hash;
 
 use crate::crush;
 use crate::result::Result;
 
-pub trait Node: Sized + PartialEq + Debug {
-    type Id: Eq + Hash + Copy + Debug;
+pub trait Node: Sized + PartialEq {
+    type Id: Eq + Hash;
 
     fn get_id(&self) -> Self::Id;
     fn get_next(&self) -> Option<Vec<Self>>;
